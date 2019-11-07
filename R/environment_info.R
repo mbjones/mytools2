b8933600b92e1a262273d7025aab422c1bf93cca
Matt Jones <gitcode@magisa.org>
2019-11-06 16:19:51 -0800

    Initial structure for mytools.

---
+++ R/environment_info.R
@@ -0,0 +1,12 @@
+#' Environment Info
+#'
+#' This prints the session and a message.
+#' @param msg The message to be printed
+#' @import devtools
+#' @export
+#' @examples
+#' environment_info("Hello!")
+environment_info <- function(msg) {
+    print(devtools::session_info())
+    print(paste("Also print the incoming message: ", msg))
+}
